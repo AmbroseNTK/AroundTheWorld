ac06d416d5b783a23cf3dc8b1db3877fe5b47270
Ambrose Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2018-04-11 12:23:48 +0700

    Add Country and World Map

--- app/src/main/java/ntk/ambrose/aroundtheworld/FlagModeActivity.java
+++ app/src/main/java/ntk/ambrose/aroundtheworld/FlagModeActivity.java
@@ -3,11 +3,27 @@ package ntk.ambrose.aroundtheworld;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class FlagModeActivity extends AppCompatActivity{
+    TextView tvQuestionNum;
+    ImageView imgFlag;
+    Button btAnsA;
+    Button btAnsB;
+    Button btAnsC;
+    Button btAnsD;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.flag_mode_activity);
+        tvQuestionNum=findViewById(R.id.tvQuestionNum);
+        imgFlag=findViewById(R.id.imgFlag);
+        btAnsA=findViewById(R.id.btAnsA);
+        btAnsB=findViewById(R.id.btAnsB);
+        btAnsC=findViewById(R.id.btAnsC);
+        btAnsD=findViewById(R.id.btAnsD);
     }
 }
---
+++ app/src/main/java/ntk/ambrose/aroundtheworld/WorldMap.java
@@ -0,0 +1,64 @@
+package ntk.ambrose.aroundtheworld;
+
+import android.content.Context;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class WorldMap {
+    public final int WIDTH = 12;
+    public final int HEIGHT = 6;
+    public ArrayList<Country>[][] map;
+    private WorldMap() {
+        map = new ArrayList[HEIGHT][WIDTH];
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                map[i][j] = new ArrayList<>();
+            }
+        }
+    }
+    private static WorldMap instance;
+    public static WorldMap getInstance(){
+        if(instance==null)
+            instance=new WorldMap();
+        return instance;
+    }
+    public void createWorldMap(Context context){
+        try {
+            InputStream inputStream = context.getAssets().open("world.xml");
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = documentBuilder.parse(inputStream);
+            Element element =document.getDocumentElement();
+            element.normalize();
+            NodeList nodeList = document.getElementsByTagName("world");
+            Node node;
+            for(int i=0;i<nodeList.getLength();i++){
+                node = nodeList.item(i);
+                if(node.getNodeType()==Node.ELEMENT_NODE){
+                    Element subElement = (Element)node;
+                    Country country = new Country(getNodeValue("name",subElement),getNodeValue("code",subElement),getNodeValue("lang",subElement));
+                    int x = Integer.parseInt(getNodeValue("x",subElement));
+                    int y = Integer.parseInt(getNodeValue("y",subElement));
+                    map[y-1][x-1].add(country);
+                }
+            }
+            inputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private static String getNodeValue(String tag, Element element) {
+        NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+        Node node = nodeList.item(0);
+        return node.getNodeValue();
+    }
+}
