86e76e03ea008131283fb1bd77172469524c3ac6
Ambrose Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2018-04-11 09:17:09 +0700

    Add Flag mode and Country Name mode layout

---
+++ app/src/main/java/ntk/ambrose/aroundtheworld/FlagModeActivity.java
@@ -0,0 +1,13 @@
+package ntk.ambrose.aroundtheworld;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class FlagModeActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flag_mode_activity);
+    }
+}
